3a9e0d26cd08ff40cc3910896d79ddb849896ef1
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-07-20 16:26:35 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,14 +25,12 @@ kotlin {
     }
 
     sourceSets {
-        val wasmJsMain by getting {
-            dependencies {
-                implementation(compose.runtime)
-                implementation(compose.foundation)
-                implementation(compose.material3)
-                implementation(compose.ui)
-                implementation(compose.components.resources)
-            }
+        wasmJsMain.dependencies {
+            implementation(compose.runtime)
+            implementation(compose.foundation)
+            implementation(compose.material3)
+            implementation(compose.ui)
+            implementation(compose.components.resources)
         }
     }
 }
