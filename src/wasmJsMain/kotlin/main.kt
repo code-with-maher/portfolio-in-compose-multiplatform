c4a605ea79cb92142ed7952c168a4bd45dad2e4a
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-07-20 16:09:50 +0300

    Create src/wasmJsMain/kotlin/main.kt

---
+++ src/wasmJsMain/kotlin/main.kt
@@ -0,0 +1,47 @@
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.material3.Button
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.CanvasBasedWindow
+
+fun main() {
+    CanvasBasedWindow(canvasElementId = "ComposeTarget") {
+        MaterialTheme {
+            Surface(
+                modifier = Modifier.fillMaxSize(),
+                color = MaterialTheme.colorScheme.background
+            ) {
+                var counter by remember { mutableStateOf(0) }
+
+                Column(
+                    modifier = Modifier.fillMaxSize(),
+                    verticalArrangement = Arrangement.Center,
+                    horizontalAlignment = Alignment.CenterHorizontally
+                ) {
+                    Text(
+                        text = "تهانينا! لقد أنشأت أول مشروع Compose Multiplatform ناجح على الويب 🚀",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+
+                    Spacer(modifier = Modifier.height(24.dp))
+
+                    Button(onClick = { counter++ }) {
+                        Text(text = "تم الضغط: $counter مرة")
+                    }
+                }
+            }
+        }
+    }
+}
