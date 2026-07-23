6ca2924a5f26ddff3afaa2a2c90acc10cdce5bc3
Maher <77703949+code-with-maher@users.noreply.github.com>
2026-07-23 15:20:44 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,14 +2,20 @@ pluginManagement {
     repositories {
         google()
         mavenCentral()
+        gradlePluginPortal()
     }
 }
 
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
+
     repositories {
         google()
         mavenCentral()
+        maven {
+            name = "NodeDistributions"
+            url = uri("https://nodejs.org/dist")
+        }
     }
 }
 
