be1b797a30040a9d59b625df02e0f0e49d717258
code-with-maher <code-with-maher@users.noreply.github.com>
2026-07-30 07:31:28 +0000

    deploy: c6de10bd19cf6bdec13e995a0a3e8534a44bef66

--- tasks/_portfolio-in-compose-multiplatform_buildWasmJsAppWasmJsRelease/import-map-loader.js
+++
@@ -1,8 +0,0 @@
-const script = document.createElement('script');
-script.type = 'importmap';
-script.textContent = JSON.stringify({
-    "imports": {
-        "@js-joda/core": "./vendors/@js-joda/core/dist/js-joda.esm.js"
-    }
-});
-document.currentScript.after(script);
